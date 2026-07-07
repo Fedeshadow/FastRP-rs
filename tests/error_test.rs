fc3c4d3f5355dd977b8c8003fda12f86c9111cf2
Federico Argilli <85882770+Fedeshadow@users.noreply.github.com>
2026-07-07 13:58:03 +0200

    Create error_test.rs

---
+++ tests/error_test.rs
@@ -0,0 +1,81 @@
+use fastrp::{FastRPBuilder, FastRPError};
+
+#[test]
+fn test_capacity_overflow() {
+    let num_nodes = 2;
+    // this dim will cause num_nodes.checked_mul(dim) to overflow
+    let dim = usize::MAX;
+    
+    let adj_list = vec![
+        vec![],
+        vec![],
+    ];
+    
+    let builder = FastRPBuilder::new(dim);
+    let result_csr = builder.fit_csr(adj_list.clone());
+    
+    assert!(result_csr.is_err());
+    match result_csr.unwrap_err() {
+        FastRPError::ShapeMismatch(msg) => {
+            assert_eq!(msg, "Capacity overflow");
+        }
+        _ => panic!("Expected ShapeMismatch"),
+    }
+    
+    let result_dense = builder.fit_dense(num_nodes, adj_list);
+    assert!(result_dense.is_err());
+    match result_dense.unwrap_err() {
+        FastRPError::ShapeMismatch(msg) => {
+            assert_eq!(msg, "Capacity overflow");
+        }
+        _ => panic!("Expected ShapeMismatch"),
+    }
+}
+
+#[test]
+fn test_oom_error() {
+    let num_nodes = 1;
+    // this dim won't overflow checked_mul, but is too large to allocate in memory
+    // We request enough elements so that it's just under isize::MAX bytes (to avoid CapacityOverflow panic)
+    // but way more memory than any machine has (e.g., 1 Exabyte).
+    let dim = usize::MAX / 16 - 1;
+    
+    let adj_list = vec![
+        vec![],
+    ];
+    
+    let builder = FastRPBuilder::new(dim);
+    
+    let result_csr = builder.fit_csr(adj_list.clone());
+    assert!(result_csr.is_err());
+    match result_csr.unwrap_err() {
+        FastRPError::Oom(_) => {} // Expected
+        err => panic!("Expected Oom, got {:?}", err),
+    }
+    
+    let result_dense = builder.fit_dense(num_nodes, adj_list);
+    assert!(result_dense.is_err());
+    match result_dense.unwrap_err() {
+        FastRPError::Oom(_) => {} // Expected
+        err => panic!("Expected Oom, got {:?}", err),
+    }
+}
+
+#[test]
+fn test_empty_graph() {
+    let num_nodes = 0;
+    let dim = 10;
+    let adj_list: Vec<Vec<(usize, f64)>> = vec![];
+    
+    let builder = FastRPBuilder::new(dim);
+    
+    // CSR
+    let result_csr = builder.fit_csr(adj_list.clone());
+    assert!(result_csr.is_ok());
+    assert_eq!(result_csr.unwrap().shape(), &[0, dim]);
+
+    // Dense
+    let result_dense = builder.fit_dense(num_nodes, adj_list);
+    assert!(result_dense.is_ok());
+    assert_eq!(result_dense.unwrap().shape(), &[0, dim]);
+}
